d9fe2f2b3945b12b5668c39f8d84b58359cd417c
Dean Butcher <dean.butcher@accuweather.com>
2024-07-08 23:19:18 -0400

    Removed comments.

--- Exercises/Chapter00/SecondVAO/main.c
+++ Exercises/Chapter00/SecondVAO/main.c
@@ -70,7 +70,6 @@ int main() {
     return 1;
   }
 
-  /* Version 4.1 Core is a good default that should run on just about everything. Adjust later to suit project requirements. */
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -84,48 +83,32 @@ int main() {
   }
   glfwMakeContextCurrent(window);
 
-  /* start GLEW extension handler */
   glewExperimental = GL_TRUE;
   glewInit();
 
-  /* get version info */
+
   renderer = glGetString(GL_RENDERER); /* get renderer string */
   version  = glGetString(GL_VERSION);  /* version as a string */
   printf("Renderer: %s\n", renderer);
   printf("OpenGL version supported %s\n", version);
 
-  /* tell GL to only draw onto a pixel if the shape is closer to the viewer
-  than anything already drawn at that pixel */
   glEnable(GL_DEPTH_TEST); /* enable depth-testing */
-  /* with LESS depth-testing interprets a smaller depth value as meaning "closer" */
+
   glDepthFunc(GL_LESS);
 
-  /* a vertex buffer object (VBO) is created here. this stores an array of
-  data on the graphics adapter's memory. in our case - the vertex points */
   glGenBuffers(1, &vbo1);
   glBindBuffer(GL_ARRAY_BUFFER, vbo1);
   glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), points0, GL_STATIC_DRAW);
 
-  /* Second vbo for other half of vertex array. */
   glGenBuffers(1, &vbo2);
   glBindBuffer(GL_ARRAY_BUFFER, vbo2);
   glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), points1, GL_STATIC_DRAW);
 
-  /* the vertex array object (VAO) is a little descriptor that defines which
-  data from vertex buffer objects should be used as input variables to vertex
-  shaders. in our case - use our only VBO, and say 'every three floats is a
-  variable' */
   glGenVertexArrays(1, &vao1);
   glBindVertexArray(vao1);
-  /* "attribute #0 should be enabled when this vao1 is bound" */
   glEnableVertexAttribArray(0);
-  /* this VBO is already bound, but it's a good habit to explicitly specify which
-  VBO's data the following vertex attribute pointer refers to */
   glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-  /* "attribute #0 is created from every 3 variables in the above buffer, of type
-  float (i.e. make me vec3s)" */
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-  //glDisableVertexAttribArray(0);
 
   /* Second VAO */
   glGenVertexArrays(1, &vao2);
@@ -133,12 +116,7 @@ int main() {
   glEnableVertexAttribArray(1);
   glBindBuffer(GL_ARRAY_BUFFER, vbo2);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-  //glDisableVertexAttribArray(1);
 
-  /* here we copy the shader strings into GL shaders, and compile them. we
-  then create an executable shader 'program' and attach both of the compiled
-      shaders. we link this, which matches the outputs of the vertex shader to
-  the inputs of the fragment shader, etc. and it is then ready to use */
   vert_shader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vert_shader, 1, &pvertex_shader, NULL);
   glCompileShader(vert_shader);
@@ -150,33 +128,21 @@ int main() {
   glAttachShader(shader_programme, vert_shader);
   glLinkProgram(shader_programme);
 
-  /* this loop clears the drawing surface, then draws the geometry described
-      by the VAO onto the drawing surface. we 'poll events' to see if the window
-  was closed, etc. finally, we 'swap the buffers' which displays our drawing
-      surface onto the view area. we use a double-buffering system which means
-      that we have a 'currently displayed' surface, and 'currently being drawn'
-      surface. hence the 'swap' idea. in a single-buffering system we would see
-      stuff being drawn one-after-the-other */
   while (!glfwWindowShouldClose(window)) {
-    /* wipe the drawing surface clear */
+    
     glClearColor(0.6f, 0.6f, 0.8f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glUseProgram(shader_programme);
     glBindVertexArray(vao1);
-    /* draw points 0-3 from the currently bound VAO with current in-use shader */
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    /* Second VAO */
     glBindVertexArray(vao2);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    /* update other events like input handling */
     glfwPollEvents();
-    /* put the stuff we've been drawing onto the display */
     glfwSwapBuffers(window);
   }
 
-  /* close GL context and any other GLFW resources */
   glfwTerminate();
   return 0;
 }
